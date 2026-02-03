5d296a6b4733c98fa59d88a53e88b3f3836e8cd1
umerwe <uemyy1@gmail.com>
2026-02-03 11:34:17 +0500

    added suspense for params

--- src/app/(site)/seller/page.tsx
+++ src/app/(site)/seller/page.tsx
@@ -1,47 +1,10 @@
-"use client"
+import { Suspense } from "react";
+import SellerPage from "@/components/pages/seller/seller";
 
-import { useSearchParams } from "next/navigation";
-import Container from "@/components/container";
-import Hero from "@/components/pages/seller/hero";
-import Tabs from "@/components/pages/seller/tabs";
-import Footer from "@/components/footer";
-import { useGetProducts } from "@/hooks/useProduct";
-import GridCard from "@/components/cards/grid-card";
-
-export default function SellerPage() {
-  const searchParams = useSearchParams();
-
-  const activeTab = searchParams.get("tab")?.toLowerCase() || "motorcycles";
-
-  const isAccessories = activeTab === "gear" || activeTab === "accessories";
-
-  const { data, isLoading } = useGetProducts({
-    type: isAccessories ? "accessories" : "motor_bike",
-    per_page: 8
-  });
-
-  const displayData = data?.data || [];
-
+export default function Page() {
   return (
-    <section>
-      <Hero />
-      <Tabs />
-
-      <Container className="pt-[40px] pb-[198px]">
-        <div className="mb-[30px]">
-          <h2 className="text-[24px] font-bold text-[#111111]">
-            Active Listings
-          </h2>
-        </div>
-
-        <GridCard
-          products={displayData}
-          isLoading={isLoading}
-          count={8}
-          isSecond={true}
-        />
-      </Container>
-      <Footer />
-    </section>
+    <Suspense fallback={<div className="h-[400px]" />}>
+      <SellerPage />
+    </Suspense>
   );
 }
