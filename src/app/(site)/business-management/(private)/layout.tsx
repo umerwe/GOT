bc422fdff82d377b149be761bc486cb82d641090
Umer Farooq Shahi <uemyy1@gmail.com>
2026-02-24 15:49:41 +0500

    removed check

--- src/app/(site)/business-management/(private)/layout.tsx
+++ src/app/(site)/business-management/(private)/layout.tsx
@@ -1,12 +1,11 @@
 "use client";
 
 import { ReactNode } from "react";
-import BusinessAuthPrivate from "@/components/auth/business-auth-private";
 
 interface BusinessAuthPrivateProps {
     children: ReactNode;
 }
 
 export default function BusinessPrivateLayout({ children }: BusinessAuthPrivateProps) {
-    return <BusinessAuthPrivate>{children}</BusinessAuthPrivate>;
+    return {children};
 }
