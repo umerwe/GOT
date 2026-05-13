d0b7f6d137516db23bbc4e83646c653a9a2d1e17
umerwe <uemyy1@gmail.com>
2026-05-13 14:31:36 +0500

    fixed brand params

--- src/hooks/useBrand.ts
+++ src/hooks/useBrand.ts
@@ -1,7 +1,7 @@
 import { useQuery } from "@tanstack/react-query";
 import { getBrand } from "@/services/brand";
 
-export const useGetBrands = (items_brand: number = 0) => {
+export const useGetBrands = (items_brand: number) => {
   return useQuery({
     queryKey: ["brands"],
     queryFn: () => getBrand(items_brand),
--- src/services/brand.ts
+++ src/services/brand.ts
@@ -1,6 +1,7 @@
 import api from "@/lib/axios";
 
-export const getBrand = async (items_brand: number = 0) => {
-  const { data } = await api.get(`/brands?items_brand=${items_brand}`);
+export const getBrand = async (items_brand?: number) => {
+  const params = items_brand ? { items_brand } : {};
+  const { data } = await api.get("/brands", { params });
   return data.data;
 };
